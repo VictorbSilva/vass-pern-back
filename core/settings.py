e51cc9039ed479b50ffbd24e4200d3783d9b51a3
Victor <victorbasilio5@gmail.com>
2026-03-22 20:56:05 -0300

    chore: ajustando origens permitidas do CORS

--- core/settings.py
+++ core/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://vass-pern-front-h9thdgoin-victorbsilvas-projects.vercel.app/produtos",
 ]
 
 REST_FRAMEWORK = {
